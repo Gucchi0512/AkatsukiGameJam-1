c410e3b5d8a5441c628e52c09567ad42c9591abe
森口大雅 <gucchi512@outlook.jp>
2019-09-14 18:12:42 +0900

    色変え実装

--- Assets/Scripts/GridColorManager.cs
+++ Assets/Scripts/GridColorManager.cs
@@ -30,13 +30,39 @@ public class GridColorManager : MonoBehaviour
     // Update is called once per frame
     public void OnUpdate()
     {
-        /*for(int i=3;i<UnitFieldData.FIELD_HEIGHT;i++)
+        for(int i=3;i<UnitFieldData.FIELD_HEIGHT;i++)
         {
             for(int j=0;j<UnitFieldData.FILED_WIDTH;j++)
             {
-                switch(m_unitFieldData.)
+                switch (m_unitFieldData.Units[i, j].GetDisplayColor())
+                {
+                    case (ColorData.Blue):
+                        m_grids[i, j].color = Color.blue;
+                        break;
+                    case (ColorData.Green):
+                        m_grids[i, j].color = Color.green;
+                        break;
+                    case (ColorData.Red):
+                        m_grids[i, j].color = Color.green;
+                        break;
+                    case (ColorData.Cyan):
+                        m_grids[i, j].color = Color.cyan;
+                        break;
+                    case (ColorData.Magenta):
+                        m_grids[i, j].color = Color.magenta;
+                        break;
+                    case (ColorData.Yellow):
+                        m_grids[i, j].color = Color.yellow;
+                        break;
+                    case (ColorData.White):
+                        m_grids[i, j].color = Color.white;
+                        break;
+                    case (ColorData.None):
+                        m_grids[i, j].color = new Color(0, 0, 0, 0);
+                        break;
+                }
             }
-        }*/
-         
+        }
+        
     }
 }
