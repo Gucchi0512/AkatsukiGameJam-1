d5f6edfa884a070075e0e9cbf5326dcac4a3aacb
Gamu2059 <yamagami0711syo@gmail.com>
2019-09-14 14:15:45 +0900

    Update : GameManagerの追加

---
+++ Assets/Scripts/ColorData.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ブロックの色情報
+/// </summary>
+public enum ColorData
+{
+    None = 0,
+    Blue = 1,
+    Green = 2,
+    Red = 4,
+    Cyan = Blue | Green,
+    Magenta = Red | Blue,
+    Yellow = Red | Green,
+    White = Red | Green | Blue,
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private LogicManager m_LogicManager;
+
+    public GameManager Instance {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            OnAwake();
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnAwake()
+    {
+        m_LogicManager = new LogicManager();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
