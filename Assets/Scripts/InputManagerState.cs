ed9e38614fef045d8904a0559e3da1a189a0f930
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-09-14 16:29:23 +0900

    [WIP] InputManagerを修正

---
+++ Assets/Scripts/InputManagerState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputManagerState
+{
+    Left, 
+
+    Right, 
+
+    Rotate, 
+
+    None, 
+
+    HardDrop, 
+
+    SoftDrop
+}
