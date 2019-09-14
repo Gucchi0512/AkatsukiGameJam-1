8e7747491d630b2effb5e2f5e4d1f78a434965ff
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-09-14 20:56:26 +0900

    PlayerPrefsでスコアを取得する機能を実装

--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ScoreHandler : MonoBehaviour
 {
+    public UnityEngine.UI.Text scoreLabel;
+    private int resultScore;
+    private int score;
+    const string RESULT_SCORE_KEY = "ResultScore";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //リザルトスコアをロード
+        score = 10230;
+        PlayerPrefs.SetInt(RESULT_SCORE_KEY,score);
+        resultScore = PlayerPrefs.GetInt(RESULT_SCORE_KEY, -1);
+        scoreLabel.text = resultScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
