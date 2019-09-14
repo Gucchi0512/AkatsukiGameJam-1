05e9eb630068642d9be6f98e219d9bd1b10e58fe
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-09-14 14:46:47 +0900

    [WIP] create InputManager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+  public GameManager gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      int state;
+      if(Input.GetKeyDown("return")) {
+        Debug.Log("Enter-String-");
+      }
+      if(Input.GetKeyDown(KeyCode.Return)) {
+        Debug.Log("Enter-KeyCode-");
+      }
+    }
