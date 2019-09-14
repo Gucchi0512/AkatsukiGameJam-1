8dca436f3c3920b8cc739c050c56c0ff71c073c8
Gamu2059 <yamagami0711syo@gmail.com>
2019-09-14 14:36:29 +0900

    Update : ブロックのデータを整備

--- Assets/Scripts/LogicManager.cs
+++ Assets/Scripts/LogicManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class LogicManager
 {
+    /// <summary>
+    /// 一人プレイ用か
+    /// </summary>
+    private bool m_IsSinglePlay;
 
-    public void OnStart()
+    public UnitFieldData FieldDataPlayer1 { get; private set; }
+    public UnitFieldData FieldDataPlayer2 { get; private set; }
+
+    public void OnStart(bool isSinglePlay)
     {
-
+        m_IsSinglePlay = isSinglePlay;
+        FieldDataPlayer1 = new UnitFieldData();
+        FieldDataPlayer2 = new UnitFieldData();
     }
 
     public void OnUpdate()
---
+++ Assets/Scripts/UnitFieldData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// フィールドとプレイヤーが動かすミノのデータなどを管理する。
+/// </summary>
+public class UnitFieldData
+{
+    private const int FILED_WIDTH = 8;
+    private const int FIELD_HEIGHT = 16;
+
+    public UnitData[,] Units { get; private set; }
+    public MinoData CurrentMino { get; private set; }
+    public MinoData NextMino { get; private set; }
+    public MinoData NextNextMino { get; private set; }
+
+    public UnitFieldData()
+    {
+        Units = new UnitData[FIELD_HEIGHT, FILED_WIDTH];
+        for (var i=0;i<FIELD_HEIGHT;i++)
+        {
+            for (var j=0;j<FILED_WIDTH;j++)
+            {
+                Units[i, j] = new UnitData();
+            }
+        }
+
+        CurrentMino = new MinoData();
+        NextMino = new MinoData();
+        NextNextMino = new MinoData();
+    }
+}
