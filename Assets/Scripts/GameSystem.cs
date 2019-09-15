f5bb29db69ecaed92b73c298c9f122c1b50eefb6
Aries1A <kaiso.mar26.littleswitch@gmail.com>
2019-09-15 15:13:20 +0900

    完成版

--- Assets/Scripts/GameSystem.cs
+++ Assets/Scripts/GameSystem.cs
@@ -14,7 +14,7 @@ public class GameSystem : MonoBehaviour
     public void Start2PGame()
     {
         PlayerPrefs.SetInt(ConstKeys.IS_SINGLE_PLAY_MODE_KEY, 1);
-        SceneManager.LoadScene("2PMainOP");
+        SceneManager.LoadScene("Main2P");
     }
 
     public void ReturnTitle()
